e4c561306231352b4be3da32b9ccd887c0fbd13f
Lee-ji-soo <nbsp3@naver.com>
2020-07-31 13:55:00 +0900

    TS-annotation-tsc

---
+++ dist/annotation.js
@@ -0,0 +1,22 @@
+function add(x, y) {
+    return x + y;
+}
+add(1, 2);
+function buildUserInfo(name, email) {
+    if (name === void 0) { name = ''; }
+    if (email === void 0) { email = ''; }
+    return { name: name, email: email };
+}
+var user = buildUserInfo();
+var add2 = function (a, b) { return a + b; };
+function store(type) {
+    if (type === "통조림") {
+        return { 일반: "통조림" };
+    }
+    else if (type === "아이스크림") {
+        return { 냉장고: "아이스크림" };
+    }
+}
+var a = store("통조림");
+console.log(a.일반);
+//# sourceMappingURL=annotation.js.map
--- src/annotation.ts
+++ src/annotation.ts
@@ -12,16 +12,20 @@ const add2 = (a: number, b: number): number => a + b;
 
 //overload
 interface Storage {
-    a: string
+    일반: string
 }
 interface ColdStorage {
-    b: string
+    냉장고: string
 }
 function store(type: "통조림"): Storage
 function store(type: "아이스크림"): ColdStorage
 
 function store(type: "통조림" | "아이스크림") {
     if (type === "통조림") {
-        return { a: "통조림" }
+        return { 일반: "통조림" }
+    } else if (type === "아이스크림") {
+        return { 냉장고: "아이스크림" }
     }
 }
+const a = store("통조림")
+console.log(a.일반)
